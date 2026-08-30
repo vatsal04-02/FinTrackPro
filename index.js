30d3916aae8a67558d64999ae702819d74042404
vatsal04-02 <vatsallink04@gmail.com>
2026-08-30 22:06:32 +0530

    login page logic fix done

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ const signupPage = document.querySelector(".signup-page");
 const signupBtn = document.querySelector("#showSignup");
 const loginBtn = document.querySelector("#showLogin");
 
+const signupName = document.querySelector("#signupName");
+const signupEmail = document.querySelector("#signupEmail");
+const signupPassword = document.querySelector("#signupPassword");
+const signupForm = document.querySelector("#signupForm");
+const loginForm = document.querySelector("#loginForm");
+
+const logoutBtn = document.querySelector("#logoutBtn");
+
+
 
 
 let TransactionsArr = [];
@@ -34,7 +43,14 @@ let TransactionsArr = [];
 let profile = { 
      name: profileName.value,
      currency: Currency.value
-    };
+};
+
+let signupInfo = {
+
+    Name: signupName.value,
+    Email: signupEmail.value,
+    Password: signupPassword.value
+}
 
 
 
@@ -198,15 +214,81 @@ function addTransactionToUI(transaction) {
 
 // login and signup navigation 
 
-signupBtn.addEventListener("click" ,()=>{
+function loginActions(){
+    
+ signupBtn.addEventListener("click" ,()=>{
     signupPage.style.display ="grid"
     loginPage.style.display = "none"
-})
+ })
 
-loginBtn.addEventListener("click", () => {
+ loginBtn.addEventListener("click", () => {
   loginPage.style.display = "grid";
   signupPage.style.display = "none";
-});
+ });
+
+
+ signupForm.addEventListener("submit",(event)=>{
+
+    signupInfo = {
+    Name: signupName.value.trim(),
+    Email: signupEmail.value.trim(),
+    Password: signupPassword.value
+ }
+
+  localStorage.setItem("fintrackUser", JSON.stringify(signupInfo));
+
+  profileName.value = signupInfo.Name;
+  usernameNav.innerText = signupInfo.Name;
+
+
+
+  
+  loginPage.style.display ="grid";
+  alert("Now please login");
+  form.reset();
+
+
+ });
+
+ loginForm.addEventListener("submit", (event) => {
+   event.preventDefault();
+   
+   const savedUser = JSON.parse(
+    localStorage.getItem("fintrackUser")
+   );
+
+    if (!savedUser) {
+    alert("Create an account before logging in.");
+    return;
+   }
+
+   const loginEmailValue = loginEmail.value.trim().toLowerCase();
+   const loginPasswordValue = loginPassword.value;
+
+   if(
+    loginEmailValue === savedUser.Email &&
+    loginPasswordValue === savedUser.Password
+   ){
+    signupPage.style.display = "none";
+    loginPage.style.display = "none";
+
+    usernameNav.innerText = savedUser.Name;
+
+    alert("Login Successful!");
+   }else{
+    alert("Email or Password is Incorrect");
+   }
+ });
+ 
+}
+
+loginActions();
+
+//logout button function
+
+logoutBtn.addEventListener("click",()=>{
+    loginPage.style.display = "grid";
+})
 
 
 
